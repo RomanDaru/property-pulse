e5fcc6be31afb4bdfe28d0914a65c55c3d7a4fa0
Roman Daru <roman.daru.ml@gmail.com>
2024-02-25 22:12:34 +0100

    Added function to Navbar Links and Dropdowns

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -8,7 +8,7 @@ const PropertyPage = () => {
   return (
     <div className="h-dvh flex justify-center items-center">
       <h1 className="text-4xl text-red-700 font-bold">
-        Welcome to PropertyPage {params.id}
+        Hello from PropertyPage {params.id}
       </h1>
     </div>
   );
