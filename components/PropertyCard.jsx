c4547d286050b028fab8ef17e77ef8b6da6612a0
Roman Daru <roman.daru.ml@gmail.com>
2024-02-29 23:06:11 +0100

    Added dynamic card link

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -69,11 +69,11 @@ const PropertyCard = ({ property }) => {
               {property.location.city} {property.location.state}{" "}
             </span>
           </div>
-          <a
-            href='property.html'
+          <Link
+            href={`/properties/${property._id}`}
             className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'>
             Details
-          </a>
+          </Link>
         </div>
       </div>
     </div>
