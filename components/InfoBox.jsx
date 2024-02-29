69e6a45f0ad514a9e492f65ff12c561dfd1f1af1
Roman Daru <roman.daru.ml@gmail.com>
2024-02-29 21:28:20 +0100

    Updated code in Infoboxes

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 const InfoBox = ({
   heading,
-  backgroundColor = "bg-gray-100",
-  buttonColor,
-  infoBoxText,
-  infoButtonText,
+  backgroundColor = 'bg-gray-100',
+  buttonInfo = {},
+  children,
 }) => {
   return (
     <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-      <h2 className="text-2xl font-bold">{heading}</h2>
-      <p className="mt-2 mb-4">{infoBoxText}</p>
+      <h2 className='text-2xl font-bold'>{heading}</h2>
+      <p className='mt-2 mb-4'>{children}</p>
       <Link
-        href="/properties"
-        className={`${buttonColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}>
-        {infoButtonText}
+        href={`${buttonInfo.link}`}
+        className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:opacity-80`}>
+        {buttonInfo.text}
       </Link>
     </div>
   );
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,25 +1,36 @@
-import React from "react";
-import InfoBox from "@/components/InfoBox";
+import React from 'react';
+import InfoBox from '@/components/InfoBox';
 
 const InfoBoxes = () => {
   return (
     <section>
-      <div className="container-xl lg:container m-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+      <div className='container-xl lg:container m-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
           <InfoBox
-            heading="For Renters"
-            infoBoxText="Find your dream rental property. Bookmark properties and contact owners."
-            infoButtonText="Browse Properties"
-            buttonColor="bg-black"
-          />
+            heading='For Renters'
+            buttonInfo={{
+              text: 'Browse Properties',
+              backgroundColor: 'bg-black',
+              link: '/properties',
+            }}>
+            <p>
+              Find your dream rental property. Bookmark properties and contact
+              owners.
+            </p>
+          </InfoBox>
           <InfoBox
-            heading="For Property Owners"
-            infoBoxText="List your properties and reach potential tenants. Rent as an
-            airbnb or long term."
-            infoButtonText="Add Property"
-            buttonColor="bg-blue-700"
-            backgroundColor="bg-blue-100"
-          />
+            heading='For Property Owners'
+            backgroundColor='bg-blue-100'
+            buttonInfo={{
+              text: 'Add Property',
+              backgroundColor: 'bg-blue-700',
+              link: '/properties/add',
+            }}>
+            <p>
+              List your properties and reach potential tenants. Rent as an
+              airbnb or long term.
+            </p>
+          </InfoBox>
         </div>
       </div>
     </section>
