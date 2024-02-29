0fbda4bceb25995941d9b1299835bc29386733de
Roman Daru <roman.daru.ml@gmail.com>
2024-02-29 17:12:10 +0100

    Created Hero.jsx, Added Hero.jsx to Main page.jax

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Link from "next/link";
+import Hero from "@/components/Hero";
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl">Welcome</h1>
-      <Link href="/properties">Show properties</Link>
-    </div>
-  );
+  return <Hero />;
 };
 
 export default HomePage;
