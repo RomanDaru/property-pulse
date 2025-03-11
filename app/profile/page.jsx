e30b0a7cf09c3eb0492ece616ace50288bec2aff
Roman Daru <roman.daru.ml@gmail.com>
2025-03-11 22:21:16 +0100

    Fixed mobile view for Profile Page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -23,9 +23,11 @@ const ProfilePage = async () => {
     <section className='bg-blue-50'>
       <div className='container m-auto py-24'>
         <div className='bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0'>
-          <h1 className='text-3xl font-bold mb-4'>Your Profile</h1>
+          <h1 className='text-3xl font-bold mb-4 text-center md:text-left'>
+            Your Profile
+          </h1>
           <div className='flex flex-col md:flex-row'>
-            <div className='md:w-1/4 mx-20 mt-10'>
+            <div className='md:w-1/4 mx-auto mt-10 mb-10'>
               <div className='mb-4'>
                 <Image
                   className='h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0'
