c912ac58167798535c28d9f081bf2d625bc49fbc
Roman Daru <roman.daru.ml@gmail.com>
2025-04-20 23:12:22 +0200

    Fix: messages.jsx

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -1,57 +1,6 @@
-import connectDB from "@/config/database";
-import Message from "@/models/Message";
-import Property from "@/models/Property";
-import { convertToSerializableObject } from "@/utils/convertToObject";
-import { getSessionUser } from "@/utils/getSessionUser";
-import MessageCard from "@/components/MessageCard";
+import Messages from "@/components/Messages";
 
-const MessagesPage = async () => {
-  await connectDB();
-
-  const sessionUser = await getSessionUser();
-  if (!sessionUser || !sessionUser.userId) {
-    throw new Error("User ID is required");
-  }
-
-  const { userId } = sessionUser;
-
-  const readMessages = await Message.find({ recipient: userId, read: true })
-    .sort({ createdAt: -1 })
-    .populate("property", "name")
-    .populate("sender", "username")
-    .lean();
-  const unreadMessages = await Message.find({ recipient: userId, read: false })
-    .sort({ createdAt: -1 })
-    .populate("property", "name")
-    .populate("sender", "username")
-    .lean();
-
-  const messages = [...unreadMessages, ...readMessages].map((messageDoc) => {
-    const message = convertToSerializableObject(messageDoc);
-    message.sender = convertToSerializableObject(messageDoc.sender);
-    message.property = convertToSerializableObject(messageDoc.property);
-    return message;
-  });
-
-  return (
-    <section className='bg-blue-50'>
-      <div className='container m-auto py-24 max-w-6xl'>
-        <div className='bg-white px-6 py-8 rounded-md shadow-md border m-4 md:m-0'>
-          <h1 className='text-3xl font-bold mb-4 text-center'>Your Messages</h1>
-
-          <div className='space-y-4'>
-            {messages.length === 0 ? (
-              <p>You have no messages.</p>
-            ) : (
-              messages.map((message) => (
-                <MessageCard key={message._id} message={message} />
-              ))
-            )}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+const MessagesPage = () => {
+  return <Messages />;
 };
-
 export default MessagesPage;
