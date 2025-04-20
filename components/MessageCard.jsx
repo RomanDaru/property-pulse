34362fd6f7e6c35a16d3d2c67285127fb7c104d0
Roman Daru <roman.daru.ml@gmail.com>
2025-04-20 21:23:29 +0200

    Added UnreadMessages and notification messages counter

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -3,21 +3,27 @@ import { useState } from "react";
 import { toast } from "react-toastify";
 import markMessageAsRead from "@/app/actions/markMessageAsRead";
 import deleteMessage from "@/app/actions/deleteMessage";
+import { useGlobalContext } from "@/context/GlobalContext";
+import { set } from "mongoose";
 
 const MessageCard = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
   const [isDeleted, setIsDeleted] = useState(false);
 
+  const { setUnreadCount } = useGlobalContext();
+
   const handleReadClick = async () => {
     const read = await markMessageAsRead(message._id);
 
     setIsRead(read);
+    setUnreadCount((prev) => (read ? prev - 1 : prev + 1));
     toast.success(`Marked as ${read ? "read" : "new"}`);
   };
 
   const handleDeleteClick = async () => {
     const deleted = await deleteMessage(message._id);
     setIsDeleted(true);
+    setUnreadCount((prev) => (isRead ? prev : prev - 1));
     toast.success("Message deleted successfully!");
   };
 
