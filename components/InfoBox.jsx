4d13da4670cea767d951e3451c6514fffce83e6c
Roman Daru <roman.daru.ml@gmail.com>
2025-04-21 01:05:24 +0200

    final fix for the day.. i am done

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,23 +1,25 @@
-import React from "react";
 import Link from "next/link";
+
+// NOTE: here we should be using Link as the navigation is withoun out
+// application
 
 const InfoBox = ({
   heading,
   backgroundColor = "bg-gray-100",
-  buttonInfo = {},
+  textColor = "text-gray-800",
+  buttonInfo,
   children,
 }) => {
   return (
     <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
-      <h2 className='text-2xl font-bold'>{heading}</h2>
-      <p className='mt-2 mb-4'>{children}</p>
+      <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+      <p className={`${textColor} mt-2 mb-4`}>{children}</p>
       <Link
-        href={`${buttonInfo.link}`}
-        className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:opacity-80`}>
+        href={buttonInfo.link}
+        className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:opacity-80`}>
         {buttonInfo.text}
       </Link>
     </div>
   );
 };
-
 export default InfoBox;
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import InfoBox from '@/components/InfoBox';
+import InfoBox from "./InfoBox";
 
 const InfoBoxes = () => {
   return (
@@ -8,10 +7,11 @@ const InfoBoxes = () => {
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
           <InfoBox
             heading='For Renters'
+            backgroundColor='bg-gray-100'
             buttonInfo={{
-              text: 'Browse Properties',
-              backgroundColor: 'bg-black',
-              link: '/properties',
+              text: "Browse Properties",
+              link: "/properties",
+              backgroundColor: "bg-black",
             }}>
             Find your dream rental property. Bookmark properties and contact
             owners.
@@ -20,17 +20,16 @@ const InfoBoxes = () => {
             heading='For Property Owners'
             backgroundColor='bg-blue-100'
             buttonInfo={{
-              text: 'Add Property',
-              backgroundColor: 'bg-blue-700',
-              link: '/properties/add',
+              text: "Add Property",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
             }}>
-            List your properties and reach potential tenants. Rent as an airbnb
-            or long term.
+            List your properties and reach potential tenants. Rent short or long
+            term.
           </InfoBox>
         </div>
       </div>
     </section>
   );
 };
-
 export default InfoBoxes;
