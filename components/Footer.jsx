252b973268205cb6f526059a7a60ba26d106fecb
Roman Daru <roman.daru.ml@gmail.com>
2025-04-27 12:52:57 +0200

    added white logo in the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import logo from "@/assets/images/logo.png";
+import logo from "@/assets/images/logo-white.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
